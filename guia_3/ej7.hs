4f650057795be4c20ce692cf7c257c685c68320c
David Wischnevsky <dwischnevsky@pampero.it.itba.edu.ar>
2024-03-18 20:08:47 -0300

    Agregar many'

--- guia_3/ej7.hs
+++ guia_3/ej7.hs
@@ -1,5 +1,10 @@
 compose = (.)
+subst f g x = f x (g x)
 
 many :: Int -> (a -> a) -> a -> a
 many 0 f = id
 many n f = compose f (many (n-1) f)
+
+many' :: Int -> (a -> a) -> (a -> a)
+many' 0 = const id
+many' n = subst compose (many (n-1))
