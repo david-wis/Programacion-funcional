6f0aa3a9148e36caaa5e3412eca7e5e4abe59e5b
davidwis <davidwis000@gmail.com>
2024-05-04 14:12:05 -0300

    Otra version de recr (usando \f x -> f x x)

--- guia_11/adicionales.hs
+++ guia_11/adicionales.hs
@@ -7,5 +7,11 @@ recr' :: b -> (a -> [a] -> b -> b) -> [a] -> b
 recr' z f xs = snd (foldr g ([], z) xs)
                where g e (es, r) = (e:es, f e es r)
 
+recr'' :: b -> (a -> [a] -> b -> b) -> [a] -> b
+recr'' z f = df (foldr rp (const z))
+           where df g y = g y y
+                 rp x h [] = f x [] (h [])
+                 rp x h (_:ys) = f x ys (h ys)
+
 foldr' :: (a -> b -> b) -> b -> [a] -> b
 foldr' f = flip recr (const . f)
